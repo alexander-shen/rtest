3967d0f00bdc883a1f5fbe917bc9711065f57138
Alexander Shen <sasha.shen@gmail.com>
2021-03-29 14:31:57 +0200

    corrections

--- robust/rtest100m.sh
+++ robust/rtest100m.sh
@@ -6,7 +6,6 @@ echo rtest -x -f $1 -e $2 -p 5000 -q5000 -n 1 -t 0 -d 1 -r 5 >> $1.test100m
 time rtest -x -f $1 -e $2 -p 5000 -q5000 -n 1 -t 0 -d 1 -r 5 >> $1.test100m
 
 # 2s
-# nor really useful with 2+2 experiments
 echo all_16 >> $1.test100m
 echo rtest -x -f $1 -e $2 -p 20 -q 20 -n 1 -t 1 -d 1 -r 1 >> $1.test100m
 time rtest -x -f $1 -e $2 -p 20 -q 20 -n 1 -t 1 -d 1 -r 1 >> $1.test100m
@@ -14,8 +13,8 @@ time rtest -x -f $1 -e $2 -p 20 -q 20 -n 1 -t 1 -d 1 -r 1 >> $1.test100m
 # 5s 8s
 echo >> $1.test100m
 echo sts_serial: >> $1.test100m
-echo rtest -x -f $1 -e $2 -p 500 -q 500 -n 10000  -t 3 -d 48 -r 1 >> $1.test100m
-time rtest -x -f $1 -e $2 -p 500 -q 500 -n 10000  -t 3 -d 48 -r 1 >> $1.test100m
+echo rtest -x -f $1 -e $2 -p 1000 -q 1000 -n 10000  -t 3 -d 48 -r 1 >> $1.test100m
+time rtest -x -f $1 -e $2 -p 1000 -q 1000 -n 10000  -t 3 -d 48 -r 1 >> $1.test100m
 
 # 20s 32s 50s
 echo >> $1.test100m
@@ -50,14 +49,14 @@ time rtest -x -f $1 -e $2 -p 2500 -q 2500 -n 200 -t 8 -d 1 -r 10 >> $1.test100m
 # 2s
 echo >> $1.test100m
 echo ent_8_16: >> $1.test100m
-echo rtest -x -f $1 -e $2 -p 500 -q 500 -n 5000 -t 9 -d 4 -r 1  >> $1.test100m
-time rtest -x -f $1 -e $2 -p 500 -q 500 -n 5000 -t 9 -d 4 -r 1 >> $1.test100m
+echo rtest -x -f $1 -e $2 -p 1000 -q 1000 -n 10000 -t 9 -d 4 -r 1  >> $1.test100m
+time rtest -x -f $1 -e $2 -p 1000 -q 1000 -n 10000 -t 9 -d 4 -r 1 >> $1.test100m
 
 # 18s 15s
 echo >> $1.test100m
 echo fftest: >> $1.test100m
-echo rtest -x -f $1 -e $2 -p 500 -q 500 -n 8192 -t 10 -d 1 -r 1  >> $1.test100m
-time rtest -x -f $1 -e $2 -p 200 -q 200 -n 8192 -t 10 -d 1 -r 1 >> $1.test100m
+echo rtest -x -f $1 -e $2 -p 2000 -q 2000 -n 8192 -t 10 -d 1 -r 1  >> $1.test100m
+time rtest -x -f $1 -e $2 -p 2000 -q 2000 -n 8192 -t 10 -d 1 -r 1 >> $1.test100m
 
 # 4s
 echo >> $1.test100m
@@ -74,14 +73,14 @@ time rtest -x -f $1 -e $2 -p 1000 -q 1000 -n 1500 -t 12 -d 25 -r 1 >> $1.test100
 # 10s 13s
 echo >> $1.test100m
 echo bitstream_o: >> $1.test100m
-echo rtest -x -f $1 -e $2 -p 100 -q 100 -n 2097171 -t 13 -d 1 -r 1 >> $1.test100m
-time rtest -x -f $1 -e $2 -p 100 -q 100 -n 2097171 -t 13 -d 1 -r 1 >> $1.test100m
+echo rtest -x -f $1 -e $2 -p 150 -q 150 -n 2097171 -t 13 -d 1 -r 1 >> $1.test100m
+time rtest -x -f $1 -e $2 -p 150 -q 150 -n 2097171 -t 13 -d 1 -r 1 >> $1.test100m
 
 # 4s 6s
 echo >> $1.test100m
 echo bitstream_n: >> $1.test100m
-echo rtest -x -f $1 -e $2 -p 10 -q 10 -n 41943040 -t 14 -d 1 -r 1 >> $1.test100m
-time rtest -x -f $1 -e $2 -p 10 -q 10 -n 41943040 -t 14 -d 1 -r 1 >> $1.test100m
+echo rtest -x -f $1 -e $2 -p 9 -q 9 -n 41943040 -t 14 -d 1 -r 1 >> $1.test100m
+time rtest -x -f $1 -e $2 -p 9 -q 9 -n 41943040 -t 14 -d 1 -r 1 >> $1.test100m
 
 # 11s 14s
 echo >> $1.test100m
