6ae3fb9182c61656a7e0a46214cef8d69529e8ac
Alexander SHEN <sasha.shen@gmail.com>
2025-09-22 22:33:13 +0200

    corrections
    Fixed postponing redrawing and other stupid errors

--- wav/graph_pipe.c
+++ wav/graph_pipe.c
@@ -58,7 +58,7 @@ void graph_end(){
 // child portion
 
 void display() {
-   glFlush();  // Render now
+   glutSwapBuffers();  // Render now
 }
 
 
@@ -71,19 +71,24 @@ void IdleEvent (){
 #define MAXBUFLEN 5
   static int buf[MAXBUFLEN];
   static int buflen=0; // at most 5 [<=4 between calls]
-  static int pipe_closed= false;
+  static bool pipe_closed= false;
   static bool inside= false; // inside Begin_End_group
   static int regime; // POINT or LINE, valid if inside
   static int cur_color;
+  long count_ops= 0;
+  #define THRESHOLD 100
   int inp; // place to read from the pipe
   int numread; // number of elements read
  
   numread= read(PIPEREAD, &inp, sizeof(int));
   while (numread==sizeof(int)){
+    count_ops++; // for screen updating
+
     pipe_closed= false;
     assert(buflen<MAXBUFLEN);
     buf[buflen]=inp; buflen++; // put the new element to buffer
     if ((buflen==2)&&(buf[0]==COLOR)){
+      printf("c");
       if (buf[1]==WHITE){glColor3f(1.0f, 1.0f, 1.0f);}
       if (buf[1]==RED){glColor3f(1.0f, 0.14f, 0.0f);}
       if (buf[1]==ORANGE){glColor3f(1.0f, 0.6f, 0.0f);}
@@ -96,7 +101,7 @@ void IdleEvent (){
     } else if ((buflen==3)&&(buf[0]==POINT)){
       // draw a point with buffer data; clear the buffer
       int x= buf[1]; int y= buf[2];
-      if ((x>=0)&&(x<WIN_WIDTH)&&(y>=0)&&(y,WIN_HEIGHT)){
+      if ((x>=0)&&(x<WIN_WIDTH)&&(y>=0)&&(y<WIN_HEIGHT)){
         if (!inside){
           inside= true;
           glBegin(GL_POINTS);
@@ -108,12 +113,18 @@ void IdleEvent (){
         }   
         assert (inside && (regime==POINT)); 
         glVertex2i(x,y);
+        // to avoid postponing redraw:
+        if ((count_ops % (long) THRESHOLD)==0){
+          glEnd();
+          glutSwapBuffers();
+          glBegin(GL_POINTS);
+        }
       }else{
         //points outside the window are skipped
       }
       buflen= 0; // clear the buffer
-    } else if ((buflen==5)&&(buf[0]=LINE)){
-    // draw a point with buffer data; clear the buffer
+    } else if ((buflen==5)&&(buf[0]==LINE)){
+    // draw a line with buffer data; clear the buffer
       int x= buf[1]; int y= buf[2]; int nx= buf[3]; int ny=buf[4];
       if ((x>=0)&&(x<WIN_WIDTH)&&(y>=0)&&(y<WIN_HEIGHT)&&
           (nx>=0)&&(nx<WIN_WIDTH)&&(ny>=0)&&(ny<WIN_HEIGHT) ){
@@ -139,8 +150,8 @@ void IdleEvent (){
   if (inside){
     glEnd();
     inside= false;
-    glFlush(); // update the screen
-  }  
+    glutSwapBuffers(); // update the screen
+  }   
   if (numread==-1){
     assert(errno == EAGAIN); // pipe is empty
   }else if (numread==0){
@@ -157,7 +168,6 @@ void IdleEvent (){
 }
  
 void main_child(int argc, char* argv[]){
-  // printf("Started\n");
   glutInit(&argc, argv); // could extract options from the command line 
                          //https://www.opengl.org/resources/libraries/glut/spec3/node10.html
   glutInitWindowSize(WIN_WIDTH,WIN_HEIGHT);
